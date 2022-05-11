38f552d4ead7d9fbee7b4f2ec6f7a32d250909db
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-05-11 13:44:32 +0200

    Update loginAppFuncions.cpp

--- loginAppFuncions.cpp
+++ loginAppFuncions.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <string.h>
-#include <regex>
 #include "loginAppFuncions.h"
 
 using namespace std;
@@ -24,26 +20,55 @@ char displayMenu()
     return option;
 }
 
+// function to make password hide while the user is typing it.
+string hidePassword(string& password){
+    char pass[250];
+    char ch;
+    int i;
+    for(i = 0;;) {
+        ch = getch();
+        // when user press enter "\n" to save the password
+        if (ch == 13) {
+            // assign last index in array of character
+            pass[i] = '\0';
+            password.assign(pass);
+            cout << endl;
+            return password;
+        }
+        else {
+            if (ch == '\b' and i > 0) { // if user press backspace
+                cout << "\b \b";
+                i -= 1;
+            }
+            else {
+                pass[i] = ch;
+                i += 1;
+                cout << '*';
+            }
+        }
+    }
+}
+
 void getPassword(string& password)
 {
     cout <<"create a password (Use 8 or more characters using a combination of letters, numbers, and symbols) : ";
-    getline(cin,password);
+    password = hidePassword(password);
     while(password.length() < 8 )
     {
         cout<<"short password! Please enter again:";
-        getline(cin,password);
+        password = hidePassword(password);
     }
 }
 
 void repeatPassword(string password, string& password2)
 {
     cout << "Re-Enter your password to confirm: ";
-    getline(cin, password2);
+    password2 = hidePassword(password);
 
     while (password2 != password)
     {
         cout <<"Passwords don't match! Please enter again: ";
-        getline(cin, password2);
+        password2 = hidePassword(password);
     }
     cout<<"Passwords match! \n";
 }
@@ -53,7 +78,7 @@ void strong_password_check(string& password)
     while(!regex_search(password,pattern))
     {
         cout<<"week password! please enter again: ";
-        getline(cin, password);
+        password = hidePassword(password);
     }
     cout<<"strong password! \n";
 
