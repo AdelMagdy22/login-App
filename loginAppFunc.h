ea1608456626298023969956e1ee070c64ece7d9
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-05-13 17:28:54 +0200

    Version 1.6

--- loginAppFunc.h
+++ loginAppFunc.h
@@ -1,6 +1,6 @@
 #ifndef LOGINAPPFUNCIONS_H_INCLUDED
 #define LOGINAPPFUNCIONS_H_INCLUDED
-#define FILE_PATH "login.txt"
+#define FILE_PATH "login.text"
 
 #include <string.h>
 #include <conio.h>
@@ -16,9 +16,8 @@
 
 using namespace std;
 
+void loodExsitingUsers(vector<string>& NA,vector<string>& ID,vector<string>& PASS,vector<string>& GM,vector<string>& NO );
 string hidePassword(string& password);
-string encript(string& enc_password);
-string decript(string& dec_password);
 char displayMenu();
 int CHECK_USRER_AND_PASS(string id ,string pass,vector<string>n_a,vector<string>i_d,vector<string>p_s,int len_c);
 bool Email_check(string email);
