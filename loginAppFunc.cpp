589528bcf9102492a8394cc65bcaccd2e6013da4
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-05-15 23:22:00 +0200

    Version 1.9

--- loginAppFunc.cpp
+++ loginAppFunc.cpp
@@ -1,6 +1,4 @@
 #include "loginAppFunc.h"
-
-
 #define FILE_PATH "login.text"
 
 
@@ -22,20 +20,20 @@ void loodExsitingUsers(vector<string>& NA,vector<string>& ID,vector<string>& PAS
         getline(data_file, line);
 
         while (!data_file.eof()) //while the end of file is NOT reached
-            {
-                getline(data_file,name,'|');
-                NA.push_back(name);
-                getline(data_file,id,'|');
-                ID.push_back(id);
-                getline(data_file,pass,'|');
-                PASS.push_back(pass);
-                getline(data_file,gmail,'|');
-                GM.push_back(gmail);
-                getline(data_file,number,'|');
-                NO.push_back(number);
-                i += 1;
-            }
-            data_file.close();
+        {
+            getline(data_file,name,'|');
+            NA.push_back(name);
+            getline(data_file,id,'|');
+            ID.push_back(id);
+            getline(data_file,pass,'|');
+            PASS.push_back(pass);
+            getline(data_file,gmail,'|');
+            GM.push_back(gmail);
+            getline(data_file,number,'|');
+            NO.push_back(number);
+            i += 1;
+        }
+        data_file.close();
     }
     else
     {
@@ -65,26 +63,32 @@ char displayMenu()
 }
 
 // function to make password hide while the user is typing it.
-string hidePassword(string& password){
+string hidePassword(string& password)
+{
     char pass[250];
     char ch;
     int i;
-    for(i = 0;;) {
+    for(i = 0;;)
+    {
         ch = getch();
         // when user press enter "\n" to save the password
-        if (ch == 13) {
+        if (ch == 13)
+        {
             // assign last index in array of character
             pass[i] = '\0';
             password.assign(pass);
             cout << endl;
             return password;
         }
-        else {
-            if (ch == '\b' and i > 0) { // if user press backspace
+        else
+        {
+            if (ch == '\b' and i > 0)   // if user press backspace
+            {
                 cout << "\b \b";
                 i -= 1;
             }
-            else {
+            else
+            {
                 pass[i] = ch;
                 i += 1;
                 cout << '*';
@@ -124,43 +128,38 @@ void repeatPassword(string password, string& password2)
 
 void strong_password_check(string& password)
 {
-    const regex pattern("([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|([A-Z]+[a-z]+[@$!%*#?&]+[\\d]*)|([A-Z]*[a-z]+[\\d]+[@$!%*#?&]+)|([A-Z]+[a-z]*[\\d]+[@$!%*#?&]+)|([A-Z]+[a-z]+[\\d]+[@$!%*#?&]+)[A-Za-z\\d@$!%*#?&]");
-    // const regex pattern("(?=.*[A-Z])(?=.*[\\d@$!%*#?&])(?=.*[A-Z\\d@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}");
-
-/*
-([a-z]+[\\d]+[A-Z]+[@$!%*#?&]*)|([a-z]+[\\d]+[@$!%*#?&]+[A-Z]*)|([a-z]+[A-Z]+[\\d]+[@$!%*#?&]*)|([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|([\\d]+[A-Z]+[a-z]+[@$!%*#?&]*)|([\\d]+[a-z]+[A-Z]+[@$!%*#?&]*)|([\\d]+[@$!%*#?&]+[A-Z]+[a-z]*)|([@$!%*#?&]+[A-Z]+[a-z]+[\\d]*)|([@$!%*#?&]+[a-z]+[A-Z]+[\\d]*)|([@$!%*#?&]+[a-z]+[\\d]+[A-Z]*)|
-
-
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-([A-Z]+[a-z]+[\\d]+[@$!%*#?&]*)|
-
-
-
-*/
-
-
-    while(!regex_match(password,pattern))
+    int sumOfCases = 0;
+    regex upperCase("[A-Z]");
+    regex lowerCase("[a-z]");
+    regex numbers("[0-9]");
+    regex SpecialCases("(?=.*[@$!%*#?&])");
+    if(regex_search(password, upperCase))
     {
-        cout<<"week password! please enter again: ";
-        password = hidePassword(password);
+        sumOfCases += 1;
     }
-    cout<<"strong password! \n";
+    else if(regex_search(password, lowerCase))
+    {
+        sumOfCases += 1;
+    }
+    else if(regex_search(password, numbers))
+    {
+    sumOfCases += 1;
+    }
+    else if(regex_search(password, SpecialCases))
+    {
+        sumOfCases += 1;
+    }
 
-    //pattern("(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
-    //return regex_match(password,pattern);
+    if(sumOfCases == 3 || sumOfCases == 4)
+    {
+        cout<<"Strong Password! \n";
+    }
+    else
+    {
+        cout<<"week password! \n";
+        getPassword(password);
+    }
 }
-
-
 
 bool Email_check(string email)
 {
@@ -186,67 +185,71 @@ bool ID_check(string ID)
     return regex_match(ID,pattern);
 }
 
-void validateUsername(vector<string>& NA, string& username)
+bool validateUsername(vector<string>& NA, string& username)
 {
     int size = NA.size();
     for(int i = 0 ; i <= size - 1; i++  )
+    {
+        while (true)
         {
-            while (true)
+            if(NA[i] == username)
             {
-                if(NA[i] == username)
-                {
-                    cout <<"This Username already exists, try another one!";
-                    getline(cin,username);
-                }else
-                {
-                    break;
-                }
+                cout <<"This Username already exists, try another one!";
+                getline(cin,username);
+            }
+            else
+            {
+                break;
             }
         }
+    }
+    return true;
 }
 
-void validateID(vector<string>& ID, string& I_D )
+bool validateID(vector<string>& ID, string& I_D )
 {
     int size = ID.size();
     for(int i = 0 ; i <= size - 1; i++  )
+    {
+        while (true)
         {
-            while (true)
+            if(ID[i] == I_D)
             {
-                if(ID[i] == I_D)
-                {
-                    cout <<"This ID already exists, try another one!";
-                    getline(cin,I_D);
-                }else
-                {
-                    break;
-                }
+                cout <<"This ID already exists, try another one!";
+                getline(cin,I_D);
+            }
+            else
+            {
+                break;
             }
         }
+    }
+    return true;
 }
 
-void validateEmail(vector<string>& GM, string& email )
+bool validateEmail(vector<string>& GM, string& email )
 {
     int size = GM.size();
     for(int i = 0 ; i <= size - 1; i++  )
+    {
+        while (true)
         {
-            while (true)
+            if(GM[i] == email)
             {
-                if(GM[i] == email)
-                {
-                    cout <<"This email already exists, try another one!";
-                    getline(cin,email);
-                }else
-                {
-                    break;
-                }
+                cout <<"This email already exists, try another one!";
+                getline(cin,email);
+            }
+            else
+            {
+                break;
             }
         }
+    }
+    return true;
 }
-
 
 void newuser()
 {
-    //saveToFile("\n");
     string x = "User name";
     string username;
     string I_D;
@@ -254,13 +257,18 @@ void newuser()
     string password2 = " ";
     string email = " ";
     char phone[11];
+    int checkValdition = 0;
 
     while(x == "User name")
     {
         cin.ignore();
         cout <<"create a username: ";
         getline(cin, username);
-        validateUsername(NA, username);
+        if(validateUsername(NA, username))
+        {
+            checkValdition += 1;
+        }
+
         if(username_check(username))
         {
             cout <<"username created successfully"<<endl;
@@ -268,17 +276,22 @@ void newuser()
             saveToFile(username);
             saveToFile("|");
             x = "ID";
-        }else
+        }
+        else
         {
             cout<<"invalid username! press enter, please "<<endl;
         }
     }
 
 
-    while (x=="ID"){
+    while (x=="ID")
+    {
         cout<<"enter your ID: ";
         cin>> I_D;
-        validateID(ID, I_D );
+        if(validateID(ID, I_D ))
+        {
+            checkValdition += 1;
+        }
         if (ID_check(I_D))
         {
             cout<<"ID is correct "<<endl;
@@ -299,14 +312,17 @@ void newuser()
         x = "Email";
     }
 
-    saveToFile(railFence(password,5,"encrypt"));
+    saveToFile(password);
     saveToFile("|");
 
     while (x == "Email")
     {
         cout<<"create an email: ";
         cin >> email;
-        validateEmail(GM, email);
+        if(validateEmail(GM, email))
+        {
+            checkValdition += 1;
+        }
         if(Email_check(email))
         {
             cout <<"email created successfully"<<endl;
@@ -319,7 +335,8 @@ void newuser()
             cout<<" invalid email"<<endl;
         }
     }
-    while (x == "Phone"){
+    while (x == "Phone")
+    {
         cout <<"enter your phone number: ";
         cin>>phone;
 
@@ -327,7 +344,7 @@ void newuser()
         {
             cout << "right phone number \n";
             saveToFile(phone);
-            saveToFile("|\n");
+            saveToFile("|");
             break;
         }
         else
@@ -335,17 +352,24 @@ void newuser()
             cout<<"invalid phone number"<<endl;
         }
     }
+
+    if (checkValdition += 1 == 3)
+    {
+        cout<<"\nRegistration complete! \n";
+    }
+    cout << "Thanks for using our App. \n" << endl;
     exit(0);
 }
 
 
 static std::string removeSpaces(std::string str)
 {
-	str.erase(remove(str.begin(), str.end(), '\n'), str.end());
-	return str;
+    str.erase(remove(str.begin(), str.end(), '\n'), str.end());
+    return str;
 }
 
-void login(vector<string>n_a,vector<string>i_d,vector<string>p_s,int len,int count){
+void login(vector<string>n_a,vector<string>i_d,vector<string>p_s,int len,int count)
+{
     string PASSWORD, I_D;
     int n =0;
     cout << "\nEnter Your Id: ";
@@ -353,33 +377,38 @@ void login(vector<string>n_a,vector<string>i_d,vector<string>p_s,int len,int cou
     cout << "Enter Your Password: " ;
     getPassword_l(PASSWORD);
 
-    if (CHECK_USRER_AND_PASS(I_D,PASSWORD,n_a,i_d,p_s,len)){
+    if (CHECK_USRER_AND_PASS(I_D,PASSWORD,n_a,i_d,p_s,len))
+    {
         n = CHECK_USRER_AND_PASS(I_D,PASSWORD,n_a,i_d,p_s,len)-1;
         cout << "Successful login, welcome "<<removeSpaces(n_a[n])<<endl;
     }
-    else {
+    else
+    {
         cout << "Failed login. Try again.";
         count ++ ;
 
-        if (count == 3){
+        if (count == 3)
+        {
             cout << "\nYou are denied access to the system....";
             exit(0);
-            // Here Put function main menu                              <--------------------------------------------------
         }
         login(n_a,i_d,p_s,len,count);
     }
 }
 
 
-void change_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<string>g_m,vector<string>n_o){
+void change_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<string>g_m,vector<string>n_o)
+{
     string id,pass = " ",new_pass;
     cout << "Enter Your Id: ";
     cin >> id;
     cout << "Enter Old Password: ";
     getPassword_l(pass);
     int size =i_d.size();
-    for (int i =0 ; i < size-1; i++){
-        if (i_d[i] == id && railFence(p_s[i],5,"decript") == pass ){
+    for (int i =0 ; i < size-1; i++)
+    {
+        if (i_d[i] == id && p_s[i] == pass )
+        {
             cout << "Enter New Password => ";
             cin.ignore();
             getPassword(p_s[i]);
@@ -393,12 +422,14 @@ void change_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<st
 }
 
 
-void save_new_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<string>g_m,vector<string>n_o){
+void save_new_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<string>g_m,vector<string>n_o)
+{
     ofstream save_file;
     save_file.open(FILE_PATH, ios::out);
     save_file << "Name" << '|'<< "ID" << '|'<< "PASSWORD" << '|'<< "Gmail" << '|' << "Number" << '\n';
     int size =i_d.size();
-    for (int i =0 ; i < size-1; i++){ // -------------------------------------------> for (int i =0 ; i < len_c; i++) ----NOT WORK
+    for (int i =0 ; i < size-1; i++)  
+    {
         save_file << n_m[i] << '|'<< i_d[i] << '|'<< p_s[i] << '|'<< g_m[i] << '|' << n_o[i] << '|';
     }
     save_file.close();
@@ -408,85 +439,15 @@ void save_new_pass(vector<string>n_m,vector<string>i_d,vector<string>p_s,vector<
 }
 
 
-int CHECK_USRER_AND_PASS(string id ,string pass,vector<string>n_a,vector<string>i_d,vector<string>p_s,int len_c){
+int CHECK_USRER_AND_PASS(string id,string pass,vector<string>n_a,vector<string>i_d,vector<string>p_s,int len_c)
+{
     int h = 0, size =i_d.size();
-    for (int i =0 ; i < size; i++){ // -------------------------------------------> for (int i =0 ; i < len_c; i++) ----NOT WORK
-        if (i_d[i] == id && (railFence(p_s[i],5,"decript")) == pass ){
+    for (int i =0 ; i < size; i++)  
+    {
+        if (i_d[i] == id && p_s[i] == pass )
+        {
             h=i+1;
         }
     }
     return h;
-}
-
-
-
-string railFence(string text, int base, string answer)
-{
-    vector<vector<string>> vectors{};
-    vector<string> tempVector{};
-    string dash{""};
-    int textLength = text.length();
-    for (int i = 0; i < textLength; i++)
-    {
-        tempVector.push_back(dash);
-    }
-    for (int i = 0; i < base; i++)
-    {
-        vectors.push_back(tempVector);
-    }
-
-    int counter{0};
-    int neg{1};
-    string value;
-    if (answer == "encrypt")
-    {
-        for (int i = 0; i < textLength; i++)
-        {
-            vectors[counter][i] = text[i];
-            counter += 1 * neg;
-            neg = (counter == base - 1 || !counter) ? neg * -1 : neg;
-        }
-        int textIndex = 0;
-        for (int i = 0; i < vectors.size(); i++)
-        {
-            for (int j = 0; j < vectors[i].size(); j++)
-            {
-                if (vectors[i][j] != dash)
-                {
-                    value += vectors[i][j];
-                    textIndex += 1;
-                }
-            }
-        }
-    }
-    else
-    {
-        for (int i = 0; i < textLength; i++)
-        {
-            vectors[counter][i] = text[i];
-            counter += 1 * neg;
-            neg = (counter == base - 1 || !counter) ? neg * -1 : neg;
-        }
-        int textIndex = 0;
-        for (int i = 0; i < vectors.size(); i++)
-        {
-            for (int j = 0; j < vectors[i].size(); j++)
-            {
-                if (vectors[i][j] != dash)
-                {
-                    vectors[i][j] = text[textIndex];
-                    textIndex += 1;
-                }
-            }
-        }
-        counter = 0;
-        neg = 1;
-        for (int i = 0; i < textLength; i++)
-        {
-            value += vectors[counter][i];
-            counter += 1 * neg;
-            neg = (counter == base - 1 || !counter) ? neg * -1 : neg;
-        }
-    }
-    return value;
 }
